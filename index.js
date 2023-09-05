30dae63d93ac840138e318f31cdcbf196a39347d
Pui Kit Li <59538991+PKLJack@users.noreply.github.com>
2023-09-05 23:02:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+const cors = require('cors');
+const express = require('express');
+const multer = require('multer');
+require('dotenv').config();
 
-var app = express();
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,10 +15,18 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  /**@type {Express.Multer.File} */
+  const file = req.file;
 
-
+  return res.json({
+    name: file.originalname,
+    type: file.mimetype,
+    size: file.size,
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log('Your app is listening on port ' + port);
 });
